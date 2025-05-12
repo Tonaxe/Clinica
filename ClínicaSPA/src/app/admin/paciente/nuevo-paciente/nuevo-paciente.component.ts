ccda8c5d2cafbb27cecb648e3c0c0d206ebca3f2
DaVinci25 <said25022004@gmail.com>
2025-05-12 10:46:51 +0200

    Logica para crear pacientes nuevos.

---
+++ ClínicaSPA/src/app/admin/paciente/nuevo-paciente/nuevo-paciente.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ApiService } from '../../../services/api.service';
+import { Router } from '@angular/router';
+import { Paciente } from '../../../models/paciente.model';
+
+@Component({
+  selector: 'app-nuevo-paciente',
+  standalone: false,
+  templateUrl: './nuevo-paciente.component.html',
+  styleUrls: ['./nuevo-paciente.component.css'] // corregido también aquí (plural)
+})
+export class NuevoPacienteComponent implements OnInit {
+  pacienteForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private apiService: ApiService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.pacienteForm = this.fb.group({
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      fecha_nacimiento: ['', Validators.required],
+      telefono: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      tipo_pago: ['PARTICULAR', Validators.required],
+      responsable_id: [null]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.pacienteForm.valid) {
+      const nuevoPaciente: Paciente = this.pacienteForm.value;
+      this.apiService.crearPaciente(nuevoPaciente).subscribe({
+        next: () => {
+          alert('Paciente creado exitosamente');
+          this.router.navigate(['/pacientes']);
+        },
+        error: (err) => {
+          console.error('Error al crear paciente:', err);
+          alert('Ocurrió un error al crear el paciente.');
+        }
+      });
+    } else {
+      alert('Por favor completa todos los campos requeridos.');
+    }
+  }
+
+  cancelar(): void {
+    this.router.navigate(['/pacientes']);
+  }
+}
